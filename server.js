175ccda5c7ae91fd7ce8269a98dc764a539557f5
LuizC <luiz@mentalmarketing.com.br>
2021-05-21 11:06:31 -0300

    Locale

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-// Load Node modules
+// Modulos essenciais
 var express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const ejs = require('ejs');
 var falaUser = require("./routes/falaUser");
 
-//Config files
+// Arqruivos de configuração de funcionamento
 process.env['NODE_CONFIG_DIR'] = __dirname + '/config/';
 console.log("Using "+process.env['NODE_CONFIG_DIR'] +" as config directory");
 var config = require("config");
 
-// Initialise Express
+// Go Express!
 var app = express();
 
-// Render static files
+// Chama os arquivos estáticos
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use("/falauser", falaUser);
 
-// Set the view engine to ejs
+// Determina que motor de renderização é o EJS
 app.set('view engine', 'ejs');
 var server = app.listen(config.get("port"), config.get("hostname"), function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log('Server running at http://' + host + ':' + port)
+    console.log('Aplicação rodando fina em http://' + host + ':' + port)
 });
 
-// Root Route
+// Root
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
